9d19fe5351ed91114005cadadc980af9d507fe37
Guru <gruchamba@gmail.com>
2025-05-23 15:51:41 +0600

    add View.OnClickListener

--- app/src/main/java/org/guru/playlistmaker/MainActivity.kt
+++ app/src/main/java/org/guru/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package org.guru.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchBtn = findViewById<Button>(R.id.searchBtn)
+        val searchBtnClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        searchBtn.setOnClickListener(searchBtnClickListener)
+
+        val libraryBtn = findViewById<Button>(R.id.libraryBtn)
+        libraryBtn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Медиатека", Toast.LENGTH_SHORT).show()
+        }
+
+        val settingsBtn = findViewById<Button>(R.id.settingsBtn)
+        settingsBtn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
