869e23015ebb559b812702aa1a88f3231208a23b
Guru <gruchamba@gmail.com>
2025-11-24 17:46:13 +0600

    add viewBinding in TrackViewHolder

--- app/src/main/java/org/guru/playlistmaker/ui/trackAdapter/TrackAdapter.kt
+++ app/src/main/java/org/guru/playlistmaker/ui/trackAdapter/TrackAdapter.kt
@@ -1,9 +1,7 @@
 package org.guru.playlistmaker.ui.trackAdapter
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import org.guru.playlistmaker.R
 import org.guru.playlistmaker.domain.models.Track
 
 class TrackAdapter(
@@ -11,10 +9,7 @@ class TrackAdapter(
     private val onClick: (Track) -> Unit
 ) : RecyclerView.Adapter<TrackViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_view_track, parent, false)
-        return TrackViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder = TrackViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         val track = tracks[position]
--- app/src/main/java/org/guru/playlistmaker/ui/trackAdapter/TrackViewHolder.kt
+++ app/src/main/java/org/guru/playlistmaker/ui/trackAdapter/TrackViewHolder.kt
@@ -1,49 +1,47 @@
 package org.guru.playlistmaker.ui.trackAdapter
 
 import android.util.Log
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import org.guru.playlistmaker.R
+import org.guru.playlistmaker.databinding.ListItemTrackViewBinding
 import org.guru.playlistmaker.domain.models.Track
 import org.guru.playlistmaker.util.dpToPx
 
-class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    private val trackImage: ImageView
-    private val trackNameView: TextView
-    private val artistNameView: TextView
-    private val durationView: TextView
+class TrackViewHolder(private val binding: ListItemTrackViewBinding) : RecyclerView.ViewHolder(binding.root) {
 
     private val TAG = "TrackViewHolder"
 
-    init {
-        trackImage = itemView.findViewById(R.id.trackImage)
-        trackNameView = itemView.findViewById(R.id.trackSongName)
-        artistNameView = itemView.findViewById(R.id.trackArtistName)
-        durationView = itemView.findViewById(R.id.trackTime)
-    }
-
     fun bind(track: Track) {
         Log.d(TAG, "Create view for track: $track")
-        
-        Glide.with(itemView)
-            .load(track.artworkUrl100)
-            .placeholder(R.drawable.ic_def_track_img)
-            .centerCrop()
-            .transform(RoundedCorners(dpToPx(2f, itemView.context)))
-            .into(trackImage)
 
-        track.trackName?.let {
-            trackNameView.text = track.trackName
-        } ?: "-"
+        binding.apply {
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .placeholder(R.drawable.ic_def_track_img)
+                .centerCrop()
+                .transform(RoundedCorners(dpToPx(2f, itemView.context)))
+                .into(trackImage)
 
-        artistNameView.text = track.artistName
-        track.trackTime?.let { durationView.text = track.trackTime }
+            track.trackName?.let {
+                trackNameView.text = track.trackName
+            } ?: "-"
 
+            artistNameView.text = track.artistName
+            track.trackTime?.let { durationView.text = track.trackTime }
+        }
+
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): TrackViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ListItemTrackViewBinding.inflate(inflater, parent, false)
+            return TrackViewHolder(binding)
+        }
     }
 
 }
