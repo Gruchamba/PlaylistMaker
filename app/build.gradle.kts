c3b90d0849ab864cba2b9ea3fc0f2f1d6c30a11f
Guru <gruchamba@gmail.com>
2025-12-02 16:45:44 +0600

    add koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,11 @@ dependencies {
     implementation(libs.glide.v4160)
 
     // https://mvnrepository.com/artifact/com.squareup.retrofit2/retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation(libs.retrofit)
 
     // https://mvnrepository.com/artifact/com.squareup.retrofit2/converter-gson
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.converter.gson)
+
+    implementation(libs.koin.android)
 
 }
