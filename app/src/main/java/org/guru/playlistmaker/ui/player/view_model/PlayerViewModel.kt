a1decd4ba29f932b90989175b6c2ff1b2ac1da5e
Guru <gruchamba@gmail.com>
2025-12-04 08:46:14 +0600

    fix PlayerState import

--- app/src/main/java/org/guru/playlistmaker/ui/player/view_model/PlayerViewModel.kt
+++ app/src/main/java/org/guru/playlistmaker/ui/player/view_model/PlayerViewModel.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import org.guru.playlistmaker.data.player.PlayerClient.PlayerState
+import org.guru.playlistmaker.domain.player.model.PlayerState
 import org.guru.playlistmaker.domain.player.PlayerInteractor
 import org.guru.playlistmaker.ui.player.activity.PlayerViewState
 import org.koin.core.component.KoinComponent
