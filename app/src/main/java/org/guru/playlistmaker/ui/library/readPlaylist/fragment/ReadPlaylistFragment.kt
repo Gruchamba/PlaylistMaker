ce58f93399f12371ebbdb9d6fcec0a1bc511c620
zalman <gruchamba@gmail.com>
2026-04-09 18:24:49 +0600

    add ui fragment_library_read_playlist

--- app/src/main/java/org/guru/playlistmaker/ui/library/readPlaylist/fragment/ReadPlaylistFragment.kt
+++ app/src/main/java/org/guru/playlistmaker/ui/library/readPlaylist/fragment/ReadPlaylistFragment.kt
@@ -1,11 +1,13 @@
 package org.guru.playlistmaker.ui.library.readPlaylist.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.guru.playlistmaker.databinding.FragmentLibraryReadPlaylistBinding
 import org.guru.playlistmaker.domain.library.playlist.model.Playlist
 import org.guru.playlistmaker.ui.library.readPlaylist.view_model.ReadPlaylistViewModel
@@ -19,6 +21,8 @@ class ReadPlaylistFragment : Fragment() {
     private val viewModel: ReadPlaylistViewModel by viewModel()
 
     private lateinit var playlist: Playlist
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     companion object {
         const val PLAYLIST_KEY = "playlist"
@@ -42,6 +46,27 @@ class ReadPlaylistFragment : Fragment() {
         requireArguments().getSerializable(PLAYLIST_KEY)?.apply {
             playlist = this as Playlist
         }
+
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.tracksBottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> { binding.overlay.visibility = View.GONE }
+                    else -> { binding.overlay.visibility = View.VISIBLE }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                binding.overlay.alpha = when {
+                    slideOffset in -1f..0f -> 0f
+                    else -> slideOffset + 0.3f
+                }
+
+                Log.d("DEBUG","slide $slideOffset alpha ${binding.overlay.alpha}")
+            }
+        })
     }
 
     override fun onDestroyView() {
